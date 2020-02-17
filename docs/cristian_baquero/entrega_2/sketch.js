b3a6c190942a7b3af008a9e3ce0af56546010cfc
Cristian <36433453+chronodio@users.noreply.github.com>
2020-02-17 12:59:55 -0500

    Update sketch.js

--- docs/cristian_baquero/entrega_2/sketch.js
+++ docs/cristian_baquero/entrega_2/sketch.js
@@ -4,11 +4,11 @@ let cr = {
   edad: 27, // Number Numerical
   gafas: true, // Boolean Categorical
   hermanos: ["Iván", "Sebastián"], // Array Categorical
-  año_de_nacimiento: 1992; // Number Ordinal
-  comida: ["Bandeja Paisa", "Tacos al Pastos", "Pozole", "Putine", "Cuchuco"] // Array Categorical
-  comic: ["Spawn", "Calvin and Hobbes", "Maüs", "One Piece"] // Array Categorical
-  favorite_historical_moments: ["Dutch Revolt", "Japan Edo Period", "Canadian XIX Century Confederation", "Old Kingdom of Egypt", "Romanian High Middle Ages", "Italian 12th and 13th centuries"] // Array Categorical
-  favorite_cellphones: ["Motorola ROKR E2", "Sony Ericsson W600", "ZTE Nubia Red Magic 3s"] // Array Categorical
+  año_de_nacimiento: 1992, // Number Ordinal
+  comida: ["Bandeja Paisa", "Tacos al Pastos", "Pozole", "Putine", "Cuchuco"], // Array Categorical
+  comic: ["Spawn", "Calvin and Hobbes", "Maüs", "One Piece"], // Array Categorical
+  favorite_historical_moments: ["Dutch Revolt", "Japan Edo Period", "Canadian XIX Century Confederation", "Old Kingdom of Egypt", "Romanian High Middle Ages", "Italian 12th and 13th centuries"], // Array Categorical
+  favorite_cellphones: ["Motorola ROKR E2", "Sony Ericsson W600", "ZTE Nubia Red Magic 3s"], // Array Categorical
 }
 
 function setup() {
