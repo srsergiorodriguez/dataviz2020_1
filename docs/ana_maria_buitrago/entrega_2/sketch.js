fa4d2b18cd03fab29afff4ccee0fe29e8f2c043c
anamago18 <48395626+anamago18@users.noreply.github.com>
2020-02-17 23:33:40 -0500

    Add files via upload

---
+++ docs/ana_maria_buitrago/entrega_2/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  // put setup code here
+}
+
+let Anamago = {
+  nombres: "Ana Maria",  // Strings
+  apellidos: "Buitrago Suarez", // Strings
+  edad: 39, // Number 
+  cabello_largo: true, // Boolean
+  hermanos: ["Juan", "Sergio", "Miguel"], // Array
+  hijos: ["Joel", "Damián"], // Array
+  sobrinos: ["Felipe", "Johana", "Camila"], // Array
+  mascota: true, // Boolean
+  peliculas_favortitas: ["Rashomon", "Amores perros", "El dictador", "Dracula", "La tumba de las luciernagas"], // Array
+  libros: ["La historia interminable", "La insoportable levedad del ser", "Persepolis", "Cronica de una muerte anunciada"], // Array 
+  deportes: false, // Boolean
+}
+function setup() {
+  console.log(Anamago);
+}
+
+function draw() {
+  // put drawing code here
+}
