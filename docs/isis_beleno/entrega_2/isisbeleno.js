f10b557587284c9973cdd4355b0e4fd21e8c194d
Isis Beleño <48398970+ibeleno@users.noreply.github.com>
2020-02-18 15:21:52 -0500

    Add files via upload

---
+++ docs/isis_beleno/entrega_2/isisbeleno.js
@@ -0,0 +1,29 @@
+let IsisBeleno = {
+    genero: "mujer", // String categóric
+    edad: 40, // number
+    identificacion: 22493628, // number
+    profesion: "comunicadora", // String categórico
+    codigo_postal: 111211, // number
+    protesis: false, // boolean
+    peso: "68 kilogramos", // string
+    declara_renta: true, // boolean
+    licencia_conducir: true,// boolean
+    Publicaciones:["Colombia: crónicas de la sin razpón pura", "Firmas de abogados en Colombia"],// Array
+    viajes:["Malasia", "Alemania", "Singapur", "Hong Kong", "Tailandia", "Ecuador", "Venezuela"], // Array
+   
+
+}
+
+
+
+
+
+
+
+
+function setup() {
+    console.log (IsisBeleno.viajes);
+
+  // put setup code here
+}
+
