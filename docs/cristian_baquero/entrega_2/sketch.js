5d33d1226ca76a2fd81dcbb675d4deb8dc81dab4
Cristian <36433453+chronodio@users.noreply.github.com>
2020-02-19 08:23:31 -0500

    Update sketch.js

--- docs/cristian_baquero/entrega_2/sketch.js
+++ docs/cristian_baquero/entrega_2/sketch.js
@@ -1,14 +1,14 @@
 let cr = {
-  nombres: "Cristian Camilo",  // String Text
-  apellido: "Baquero Vasquez", // String Text
-  edad: 27, // Number Numerical
-  gafas: true, // Boolean Categorical
-  hermanos: ["Iván", "Sebastián"], // Array Categorical
-  año_de_nacimiento: 1992, // Number Ordinal
-  comida: ["Bandeja Paisa", "Tacos al Pastos", "Pozole", "Putine", "Cuchuco"], // Array Categorical
-  comic: ["Spawn", "Calvin and Hobbes", "Maüs", "One Piece"], // Array Categorical
-  favorite_historical_moments: ["Dutch Revolt", "Japan Edo Period", "Canadian XIX Century Confederation", "Old Kingdom of Egypt", "Romanian High Middle Ages", "Italian 12th and 13th centuries"], // Array Categorical
-  favorite_cellphones: ["Motorola ROKR E2", "Sony Ericsson W600", "ZTE Nubia Red Magic 3s"], // Array Categorical
+  nombres: "Cristian Camilo",  // String Categorial Nominal
+  apellido: "Baquero Vasquez", // String Categorical Nominal
+  edad: 27, // Number Numerical Continual
+  gafas: true, // Boolean Categorical Binomial
+  hermanos: ["Iván", "Sebastián"], // Array Categorical Nominal
+  año_de_nacimiento: 1992, // Number Numerical Discrete
+  comida: ["Bandeja Paisa", "Tacos al Pastos", "Pozole", "Putine", "Cuchuco"], // Array Categorical Nominal
+  comic: ["Spawn", "Calvin and Hobbes", "Maüs", "One Piece"], // Array Categorical Nominal
+  favorite_historical_moments: ["Dutch Revolt", "Japan Edo Period", "Canadian XIX Century Confederation", "Old Kingdom of Egypt", "Romanian High Middle Ages", "Italian 12th and 13th centuries"], // Array Categorical Nominal
+  favorite_cellphones: ["Motorola ROKR E2", "Sony Ericsson W600", "ZTE Nubia Red Magic 3s"], // Array Categorical Nominal
 }
 
 function setup() {
