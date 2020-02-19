9b2d5d31dd55b2356c37025b6efb7f5c32f30c15
Tereloayza <48361026+Tereloayza@users.noreply.github.com>
2020-02-18 21:48:33 -0500

    Add files via upload

---
+++ docs/teresa_loayza/entrega_2/sketch.js
@@ -0,0 +1,65 @@
+let Objeto_Teresa ={
+  nombre: "Teresa", // String
+  apellidos: "Loayza Sánchez", // String
+  gafas: true, // Boolean
+  hijos: true, // Boolean
+  directores_cine: ["Berman", "Wenders", "Allen", "Gondry", "Miyazaki", "Almodóvar"] // String Array - lista de directores más vistos
+  animales_horoscopo: ["dragón", "león"]
+  tolerancia_picante: 8.7; // Number - En escala de 0.0 a 10.0
+  lugar_nacimiento [0.60097102, -74.081749] // Number array - Coordenadas latitud y longitud 
+  lugar_favorito [52.512210, 13.326970] // Number array - Coordenadas latitud y longitud 
+  condimentos: ["mostaza", "pimienta", "páprika", "panka", "merquén"] // String array - condimentos favoritos
+  mascotas_en_el_cielo: [["perros" , 2], ["gatos", 1] ["pájaros", 3], ["tortugas", 1], ["conejos", 2], ["peces", 3]] // String array / number - Tipo de mascota y cantidad
+  paleta_ropa: [
+    [
+      0,
+      0,
+      0
+    ],
+    [
+      255,
+      255,
+      255
+    ],
+    [
+      220,
+      214,
+      213
+    ],
+    [
+      231,
+      222,
+      25
+    ],
+    [
+      25,
+      219,
+      198
+    ],
+    [
+      249,
+      182,
+      3
+    ],
+    [
+      44,
+      129,
+      21
+    ],
+    [
+      180,
+      156,
+      76
+    ],
+    [
+      188,
+      35,
+      222
+    ],
+    [
+      185,
+      16,
+      98
+    ]
+  ]
+}
