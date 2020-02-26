f30820c7109e3cdf8b65e7385ee6f68a4c7ebd1a
Cristian <36433453+chronodio@users.noreply.github.com>
2020-02-26 07:37:27 -0500

    Update sketch.js

--- docs/cristian_baquero/entrega_2/sketch.js
+++ docs/cristian_baquero/entrega_2/sketch.js
@@ -5,7 +5,7 @@ let cr = {
   gafas: true, // Boolean Categorical Binomial
   hermanos: ["Iván", "Sebastián"], // Array Categorical Nominal
   año_de_nacimiento: 1992, // Number Numerical Discrete
-  comida: ["Bandeja Paisa", "Tacos al Pastos", "Pozole", "Putine", "Cuchuco"], // Array Categorical Nominal
+  comida: ["Bandeja Paisa", "Tacos al Pastor", "Pozole", "Putine", "Cuchuco"], // Array Categorical Nominal
   comic: ["Spawn", "Calvin and Hobbes", "Maüs", "One Piece"], // Array Categorical Nominal
   favorite_historical_moments: ["Dutch Revolt", "Japan Edo Period", "Canadian XIX Century Confederation", "Old Kingdom of Egypt", "Romanian High Middle Ages", "Italian 12th and 13th centuries"], // Array Categorical Nominal
   favorite_cellphones: ["Motorola ROKR E2", "Sony Ericsson W600", "ZTE Nubia Red Magic 3s"], // Array Categorical Nominal
